5a10368c705ab270ef29603f8b9d7fec72727793
CodyConck <codyconck@gmail.com>
2021-04-28 14:49:56 -0500

    tightened up code to match snapshot

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,26 +76,24 @@ var startGame = function() {
   playerAttack = 10;
   playerMoney = 10;
 
+  // fight each enemy robot by looping over them and fighting them one at a time
   for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
-  if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    if (playerHealth > 0) {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+      // pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
 
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
 
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
 
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
-
-    //if we're not at the last enemy in the array
-    if (playerHealth > 0 && i < enemyNames.length - 1) {
+      //if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
       //ask if player wants to use the store before next round
       var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
 
@@ -107,24 +105,22 @@ var startGame = function() {
   }
   // if player isn't alive, stop the game
     else {
-      window.alert('You have lost your robot in battle! Game Over!');
+      window.alert("You have lost your robot in battle! Game Over!");
       break;
     }
   }
   //after the loop ends, player is either out of health or enemies to fight, so run the endGame function
   endGame();
-  //play again
-  startGame();
 };
 
 //function to end entire game
 var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
   //if the player is still alive, player wins!
   if (playerHealth > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + "." );
-  }
-  else {
-    window.alert("You've lost your robot in battle.");
+  } else {
+    window.alert("You've lost your robot in battle!");
   }
   //ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
@@ -132,16 +128,16 @@ var endGame = function() {
   if (playAgainConfirm) {
     //restart the game
     startGame();
-  }
-  else {
+  } else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
 };
 
+//go to shop between battles function
 var shop = function() {
   //ask player what they would like to do
   var shopOptionPrompt = window.prompt(
-    "would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
   );
 //use switch to carry out action
 switch (shopOptionPrompt) {
@@ -157,7 +153,6 @@ switch (shopOptionPrompt) {
   else {
     window.alert("You don't have enough money!");
   }
-
     break;
   case "UPGRADE": //new case
   case "upgrade":
@@ -171,7 +166,6 @@ switch (shopOptionPrompt) {
   else {
     window.alert("You don't have enough money!");
   }
-  
     break;
   case "LEAVE": //new case
   case "leave":
